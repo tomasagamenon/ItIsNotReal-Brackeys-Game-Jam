6b858c460b5575467f74e8599d424b993bac1f7a
tomasagamenon <tomas.agamenon@gmail.com>
2022-02-26 14:48:07 -0300

    Kodak puta

--- ItIsNotReal/Assets/Scripts/LightSwitch.cs
+++ ItIsNotReal/Assets/Scripts/LightSwitch.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LightSwitch : Interactable
 {
+    private bool ligth;
     [SerializeField]
     private Material ligthOn;
     [SerializeField]
@@ -17,9 +18,31 @@ public class LightSwitch : Interactable
 
     public override void Interact()
     {
+        StopAllCoroutines();
+        if (ligth)
+            ligth = false;
+        else ligth = true;
         int numOfLight = 0;
         foreach (GameObject light in lightsRoof)
         {
+            if (ligth == true)
+            {
+                StartCoroutine(Flashing(true, light, numOfLight));
+            }
+            else
+            {
+                StartCoroutine(Flashing(false, light, numOfLight));
+            }
+            numOfLight++;
+        }
+    }
+
+    IEnumerator Flashing(bool a, GameObject light, int numOfLight)
+    {
+        var b = Random.Range(1, 10);
+        for (int i = 0; i<= b; i++)
+        {
+            float c = Random.Range(0.01f, 0.5f);
             if (lights[numOfLight].activeSelf == true)
             {
                 light.GetComponentInChildren<Renderer>().material = lightOff;
@@ -34,7 +57,21 @@ public class LightSwitch : Interactable
                 if (darkness.Count > numOfLight)
                     darkness[numOfLight].SetActive(false);
             }
-            numOfLight++;
+            yield return new WaitForSeconds(c);
+        }
+        if (a)
+        {
+            light.GetComponentInChildren<Renderer>().material = lightOff;
+            lights[numOfLight].SetActive(false);
+            if (darkness.Count > numOfLight)
+                darkness[numOfLight].SetActive(true);
+        }
+        else
+        {
+            light.GetComponentInChildren<Renderer>().material = ligthOn;
+            lights[numOfLight].SetActive(true);
+            if (darkness.Count > numOfLight)
+                darkness[numOfLight].SetActive(false);
         }
     }
 }
